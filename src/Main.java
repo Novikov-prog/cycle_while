7ad98874cca856ec188f8082a57ddbbca61a8cc9
maxim <novikovprog@gmail.com>
2021-03-07 18:17:27 +0100

    creation: console mot de passe avec le cycle while

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int secretPinCode = 7567;
+        int pinCode = -1;
+
+        Scanner scanner = new Scanner(System.in); // lecture des données depuis la console
+        while (pinCode != secretPinCode) {
+            if (pinCode == -1) {
+                System.out.println("Entrez le mot de passe:");
+            } else {
+                System.out.println("Erreur de mot de passe. Essayez encore:");
+            }
+            pinCode = scanner.nextInt();
+        }
+
+        System.out.println("Mot de passe correct!");
+    }
+}
